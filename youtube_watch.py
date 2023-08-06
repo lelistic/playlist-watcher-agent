4e7e62f5d0d6d2e84980c381a7abf9777670e7df
@eusouolelis <68086826+lelistic@users.noreply.github.com>
2023-08-06 11:57:16 -0300

    Add files via upload

--- youtube_watch.py
+++ youtube_watch.py
@@ -56,16 +56,6 @@ def get_video_links_from_playlist(playlist_link):
 
     return video_links
 
-def get_cooldown_based_on_time():
-    now = datetime.now()
-    hour = now.hour
-
-    # Define different cooldowns for different times of the day
-    if hour >= 17:  # After 5 pm
-        return random.randint(5, 10)  # Random cooldown between 5 to 10 seconds
-    else:  # In the morning
-        return random.randint(15, 30)  # Random cooldown between 15 to 30 seconds
-
 # Function to calculate the final cooldown duration based on various factors
 def calculate_cooldown_duration(video_link, proxy=None):
     # Apply dynamic adjustment to cooldown calculation based on real-time feedback
@@ -88,47 +78,11 @@ def calculate_cooldown_duration(video_link, proxy=None):
 
     return cooldown_duration
 
-def watch_playlist(playlist_link, num_repeats=1, num_videos_to_watch=5):
-    video_links = get_video_links_from_playlist(playlist_link)
-    total_videos = len(video_links)
-
-    # Introduce randomness to the cooldown duration for better pacing
-    def get_random_cooldown():
-        return random.randint(10, 20)
-
-    for repeat in range(num_repeats):
-        print(f"\n---- Repeat #{repeat + 1} ----")
-
-        # Shuffle the order of videos in the playlist for better diversification
-        random.shuffle(video_links)
-
-        for video_number, video_link in enumerate(video_links, start=1):
-            print(f"\nVideo {video_number}/{total_videos}: {video_link}")
-
-            # Apply dynamic adjustment to cooldown calculation based on real-time feedback
-            min_cooldown = 10
-            max_cooldown = 30
-            cooldown_duration = max_cooldown - (video_views.get(video_link, 0) * 2)
-            cooldown_duration = max(min(cooldown_duration, max_cooldown), min_cooldown)
-
-            # Introduce randomness to cooldown duration
-            cooldown_duration += get_random_cooldown()
-
-            # Introduce time-based variation (Overall pacing mechanism)
-            cooldown_duration += get_cooldown_based_on_time()
-
-            print(f"Cooldown: {cooldown_duration} seconds")
-            watch_video(video_link, video_number)  # Pass video_number as argument
-
-            # Update the views count for the video
-            video_views[video_link] = video_views.get(video_link, 0) + 1
-
-            time.sleep(cooldown_duration)
-
 
 def watch_video(video_link, video_number):
     path_to_webdriver = '/usr/bin/chromedriver'  # Path to ChromeDriver in the Docker container
     options = webdriver.ChromeOptions()
+
     options.add_argument('--headless')  # Run Chrome headless
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
@@ -136,28 +90,47 @@ def watch_video(video_link, video_number):
     driver = webdriver.Chrome(executable_path=path_to_webdriver, options=options)
     driver.set_page_load_timeout(10)  # Set page load timeout to 10 seconds (adjust as needed)
 
-    try:
-        driver.get(video_link)
-        
-        # Extract the video duration from the page
-        video_duration = driver.find_element(By.CSS_SELECTOR, '.ytp-time-duration').text
-        total_seconds = sum(int(x) * 60 ** i for i, x in enumerate(reversed(video_duration.split(":"))))
-        
-        print(f"Watching Video {video_number}: {video_link}")
-        print(f"Video Duration: {video_duration} (Total seconds: {total_seconds})")
-        
-        # Wait for the total duration of the video
-        for seconds_left in range(total_seconds, 0, -1):
-            sys.stdout.write(f"\rTime left: {seconds_left} seconds")
-            sys.stdout.flush()
-            time.sleep(1)
-        
-        print("\nVideo watched successfully!")
+    proxies = get_free_proxies()
 
-    except Exception as e:
-        print(f"An error occurred while watching Video {video_number}: {e}")
-    finally:
-        driver.quit()
+    retries = 0
+    max_retries = 3
+    while retries < max_retries:
+        try:
+            proxy = random.choice(proxies)
+            if proxy:
+                options.add_argument(f'--proxy-server={proxy}')
+
+            driver.get(video_link)
+            
+            # Extract the video duration from the page
+            duration_element = driver.find_element(By.CSS_SELECTOR, '.ytp-time-duration')
+            video_duration = duration_element.text
+            total_seconds = sum(int(x) * 60 ** i for i, x in enumerate(reversed(video_duration.split(":"))))
+            
+            print(f"Watching Video {video_number}: {video_link} with Proxy {proxy}")
+            print(f"Video Duration: {video_duration} (Total seconds: {total_seconds})")
+            
+            # Wait for the total duration of the video
+            for seconds_left in range(total_seconds, 0, -1):
+                sys.stdout.write(f"\rTime left: {seconds_left} seconds")
+                sys.stdout.flush()
+                time.sleep(1)
+            
+            print("\nVideo watched successfully!")
+            return True  # Proxy worked successfully
+
+        except Exception as e:
+            print(f"An error occurred while watching Video {video_number} with Proxy {proxy}: {e}")
+            retries += 1
+            if retries < max_retries:
+                print(f"Retrying video {video_number} with a new proxy...\n")
+            else:
+                print(f"Reached maximum retries for video {video_number}. Moving on to the next video.\n")
+                return False  # Proxy didn't work after max retries
+        finally:
+            driver.quit()
+
+    return False  # Proxy didn't work even after retries
 
 
 if __name__=='__main__':
@@ -165,24 +138,20 @@ if __name__=='__main__':
     video_links = get_video_links_from_playlist(playlist_link)
     total_videos = len(video_links)
 
-    proxies = get_free_proxies()
 
-    for repeat in range(15):
-        print(f"\n---- Repeat #{repeat + 1} ----")
+    # Shuffle the order of videos in the playlist for better diversification
+    random.shuffle(video_links)
 
-        # Shuffle the order of videos in the playlist for better diversification
-        random.shuffle(video_links)
+    for video_number, video_link in enumerate(video_links):
+        proxy = None
 
-        for video_number, video_link in enumerate(video_links, start=1):
-            proxy = random.choice(proxies)
+        cooldown_duration = calculate_cooldown_duration(video_link, proxy)
 
-            cooldown_duration = calculate_cooldown_duration(video_link, proxy)
-
-            print(f"\nVideo {video_number}/{total_videos}: {video_link}")
-            print(f"Cooldown: {cooldown_duration} seconds")
-
-            watch_video(video_link, video_number, proxy)
-
+        print(f"\nVideo {video_number+1}/{total_videos}: {video_link}")
+        print(f"Cooldown: {cooldown_duration} seconds")
+        if not watch_video(video_link, video_number + 1):
+            print(f"All proxies failed for video {video_number + 1}. Skipping to the next video.")
+        else:
             # Update the views count for the video
             video_views[video_link] = video_views.get(video_link, 0) + 1
 
