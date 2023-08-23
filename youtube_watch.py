9edebe159057dcb707228d6a0ea851d30ee9ea31
Claudio Lelis <lelistic@gmail.com>
2023-08-22 22:28:35 -0300

    upsell error and play button

--- youtube_watch.py
+++ youtube_watch.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+
 from proxy_utils import get_driver ,curated_proxies
 import time
 
@@ -56,10 +58,20 @@ def watch_video(video_link, video_number):
         )
 
         if not is_playing:
-            # Click the video play button to start playback
-            play_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ytp-large-play-button')))
-            #play_button = driver.find_element(By.CSS_SELECTOR, 'button.ytp-large-play-button')
-            play_button.click()
+            try:
+                # Click the video play button to start playback
+                play_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ytp-large-play-button')))
+                #play_button = driver.find_element(By.CSS_SELECTOR, 'button.ytp-large-play-button')
+                play_button.click()
+            except ElementClickInterceptedException as e1:
+                if 'yt-upsell-dialog-renderer' in repr(e1):
+                    print("Detected an upsell dialog, attempting to close...")
+                    driver.execute_script('document.querySelector(".yt-upsell-dialog-renderer").style.display = "none";')
+                    print("Upsell dialog closed. Trying click again...")
+                    play_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ytp-large-play-button')))
+                    play_button.click()
+                else:
+                    raise e1
 
         # Calculate a random start point within the range of 22% to 47%
         start_point = total_seconds * 0.22 + (total_seconds * 0.25 * random.random())
@@ -71,6 +83,27 @@ def watch_video(video_link, video_number):
         progress_width = start_point / total_seconds
         driver.execute_script("arguments[0].style.transform = 'scaleX({})'".format(progress_width), progress_element)
 
+        # Check if the video is playing or paused
+        is_playing = driver.execute_script(
+            "return document.querySelector('.html5-video-player').paused === false;"
+        )
+
+        if not is_playing:
+            try:
+                # Click the video play button to start playback
+                play_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ytp-large-play-button')))
+                #play_button = driver.find_element(By.CSS_SELECTOR, 'button.ytp-large-play-button')
+                play_button.click()
+            except ElementClickInterceptedException as e2:
+                if 'yt-upsell-dialog-renderer' in repr(e2):
+                    print("Detected an upsell dialog, attempting to close...")
+                    driver.execute_script('document.querySelector(".yt-upsell-dialog-renderer").style.display = "none";')
+                    print("Upsell dialog closed. Trying click again...")
+                    play_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ytp-large-play-button')))
+                    play_button.click()
+                else:
+                    raise e2
+
         print("Watching video with Proxy:", proxy)
         print("Video started from {:.2f}%".format(start_point / total_seconds * 100))
         
@@ -81,8 +114,8 @@ def watch_video(video_link, video_number):
         result_ = True  # Proxy worked successfully
         
 
-    except Exception as e:
-        print("Error trying to watch video", video_number, "with Proxy",proxy,"|", repr(e))
+    except Exception as e3:
+        print("Error trying to watch video", video_number, "with Proxy",proxy,"|", repr(e3))
     finally:
         if driver:
             driver.quit()
